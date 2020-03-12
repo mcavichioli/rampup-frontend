5a504630c3dc6e04407e7a2428d1de9f519b7174
Mariana Cavichioli <cavichiolimariana@gmail.com>
2020-03-12 15:00:58 -0300

    Create cart page

--- src/pages/cart/Cart.js
+++ src/pages/cart/Cart.js
@@ -1,14 +1,8 @@
 import React from 'react';
 import './Cart.css';
-import { Provider } from 'react-redux'
-import cartProducts from '../products/Products.js'
 
 export default function Cart() {
     return(
-        <Provider store={cartProducts}>
-            console.log();
-            
-            <div>Cart</div>
-        </Provider>
+        <div>Cart</div>
     );
 }
