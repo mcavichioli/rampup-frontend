3c858f0d34b88686ef61edd68e83be55b537f21a
Mariana <cavichiolimariana@gmail.com>
2020-03-14 11:07:59 -0300

    Fix eslint problem

--- src/pages/cart/Cart.js
+++ src/pages/cart/Cart.js
@@ -11,8 +11,6 @@ export default function Cart() {
     }
 
     function removeProductfromCart(product) {
-        // console.log(products.filter(product => product === productToRemove));
-        // products.pop(products.filter(product => product === productToRemove));
         dispatch({type: 'REMOVE_PRODUCTS', product});
     }
 
@@ -21,7 +19,10 @@ export default function Cart() {
             {products.map(product => (
                 <article key={product._id}>
                     <strong>{product.name}</strong>
-                    <button className="btn" onClick={() => removeProductfromCart(product)}><i className="fas fa fa-fw fa-times"></i></button>
+                    <button className="btn" 
+                        onClick={() => removeProductfromCart(product)}>
+                            <i className="fas fa fa-fw fa-times"></i>
+                    </button>
                     <p>{product.description}</p>
                     <strong>R$ {product.price}</strong>
                 </article>
--- src/shared/components/Header/Header.js
+++ src/shared/components/Header/Header.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import './Header.css';
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 
 // Stateless component: criar componentes em react com apenas funções, ao invés de class
-const Header = () => (
-    <div className="navbar">
-        <Link to="/" className="active" ><i className="fa fa-fw fa-home"></i> Products </Link>
-        <Link to="/cart"><i className="fa fa-fw fa-shopping-cart"></i> Cart</Link>
-        <Link to="/orders"><i className="fa fa-fw fa-shopping-bag"></i> Orders</Link>
-    </div>
-);
-
-export default Header;
+export default function Header() {
+    const products = useSelector(state => state.data);
+    return (
+        <div className="navbar">
+            <Link to="/" className="active" ><i className="fa fa-fw fa-home"></i> Products </Link>
+            <Link to="/cart"><i className="fa fa-fw fa-shopping-cart"></i> 
+                Cart <small>({products.length !== 0 ? products.length : '0'})</small>
+            </Link>
+            <Link to="/orders"><i className="fa fa-fw fa-shopping-bag"></i> Orders</Link>
+        </div>
+    );
+}
